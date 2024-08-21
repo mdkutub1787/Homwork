95befab46a313b8146c3aca83972f2fe00230e1e
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-08-21 16:31:53 +0600

    add

---
+++ Spring-Boot/SpringBoot/src/main/java/com/kutub/SpringBoot/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.kutub.SpringBoot.service;
+
+import com.kutub.SpringBoot.entity.Student;
+import com.kutub.SpringBoot.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    public void saveStudent(Student s) {
+        studentRepository.save(s);
+    }
+
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    public void deleteStudentById(int id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student findStudentById(int id) {
+        return studentRepository.findById(id).get();
+
+    }
+}
