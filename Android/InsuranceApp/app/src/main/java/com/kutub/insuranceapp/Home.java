b4c9d6ff9162d35b28a7839366a01333ea75b1d6
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-07 11:44:20 +0600

    add

---
+++ Android/InsuranceApp/app/src/main/java/com/kutub/insuranceapp/Home.java
@@ -0,0 +1,59 @@
+package com.kutub.insuranceapp;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        TextView textView = findViewById(R.id.textView2);
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPhone = findViewById(R.id.editTextPhone);
+        RadioGroup radioGroupGender = findViewById(R.id.radioGroupGender);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String phone = editTextPhone.getText().toString();
+
+
+                int selectedGenderId = radioGroupGender.getCheckedRadioButtonId();
+                RadioButton selectedGender = findViewById(selectedGenderId);
+                String gender = selectedGender != null ? selectedGender.getText().toString() : "Not Selected";
+
+
+                textView.setText("Name: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nGender: " + gender);
+            }
+        });
+
+    }
+}
